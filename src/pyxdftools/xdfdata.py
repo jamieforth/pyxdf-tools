561f133aaabf472f54cd9756ddcd142decf167d5
Jamie Forth <j.forth@gold.ac.uk>
2023-10-24 22:06:01 +0100

    add `data` to data_channel_types

--- src/pyxdftools/xdfdata.py
+++ src/pyxdftools/xdfdata.py
@@ -35,7 +35,7 @@ class XdfData (RawXdf):
     # https://mne.tools/stable/glossary.html#term-data-channels
     __data_channel_types = ['mag', 'grad', 'eeg', 'csd', 'seeg', 'ecog', 'dbs'
                             'hbo', 'hbr', 'fnirs_cw_amplitude', 'fnirs_fd_ac_amplitude',
-                            'fnirs_fd_phase', 'fnirs_od']
+                            'fnirs_fd_phase', 'fnirs_od'] + ['data']
 
     __microvolts = ('microvolt', 'microvolts', 'uV', 'µV', 'μV')
 
